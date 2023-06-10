657d5185212d571d8b0cb21f8747349f267e2c47
Ignacio Lopezosa <ignacio.lopezosa@ebury.com>
2023-06-10 22:23:17 +0200

    connect to db

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,13 @@ export default registerAs('config', () => {
       name: process.env.DATABASE_NAME,
       port: process.env.DATABASE_PORT,
     },
+    postgres: {
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 10),
+      user: process.env.POSTGRES_USER,
+      pass: process.env.POSTGRES_PASSWORD,
+      name: process.env.POSTGRES_DB,
+    },
     apiKey: process.env.API_KEY,
   };
 });
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,5 +1,7 @@
 import { Module, Global } from '@nestjs/common';
-
+import { ConfigType } from '@nestjs/config';
+import { Client } from 'pg';
+import config from '../config';
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
 
@@ -10,7 +12,24 @@ const API_KEY_PROD = 'PROD1212121SA';
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
     },
+    {
+      provide: 'PG',
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { user, host, name, pass, port } = configService.postgres;
+        const client = new Client({
+          user,
+          host,
+          database: name,
+          password: pass,
+          port,
+        });
+
+        client.connect();
+        return client;
+      },
+      inject: [config.KEY],
+    },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY', 'PG'],
 })
 export class DatabaseModule {}
